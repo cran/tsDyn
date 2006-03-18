6d9af90bd191a5ab2d9a52369834ded321a1eeb5
Antonio, Fabio Di Narzo <antonio.dinarzo@studio.unibo.it>
2006-03-18 00:00:00 +0000

    version 0.5-2

---
+++ src/misc.c
@@ -0,0 +1,7 @@
+/*Author: Antonio, Fabio Di Narzo. Last Modified $Date: 2006-04-26 23:09:52 +0200 (mer, 26 apr 2006) $*/
+/*
+Global utility variables used by macros in 'tseriesChaos.h'
+*/
+
+int MVCONV_i, MVCONV_j;
+int DIST2_i;
